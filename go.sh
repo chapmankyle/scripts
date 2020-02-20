eaf1cde89cde9a1a82f7081fade9c28f0801b9c8
Kyle Chapman <20703236@sun.ac.za>
2020-02-20 21:33:13 +0200

    [ADD] Go installation

---
+++ go.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Bash script to install Go version 1.13.8
+
+echo -e "\033[92mDownloading Go 1.13.8\033[0m"
+# curl -O https://storage.googleapis.com/golang/go1.13.8.linux-amd64.tar.gz
+
+echo -e "\n\033[93mMoving compressed file to /usr/local and unzipping\033[0m"
+# sudo tar -C /usr/local -xzf go1.13.8.linux-amd64.tar.gz
+
+echo -e "\n\033[94mAdding Go to PATH\033[0m"
+sudo chown -R $USER /etc/profile
+sudo echo "export GOPATH=$HOME/go" >> /etc/profile
+sudo echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> /etc/profile
+
+# rm -f go1.13.8.linux-amd64.tar.gz
+
+echo -e "\n\033[92mSuccess!\033[0m"
+echo -e "Now type \033[93msource ~/.profile\033[0m in order to apply changes."
